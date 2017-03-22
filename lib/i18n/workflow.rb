96d69884e49f856df3ef946f7f239b15f9e91532
Priit Tark <priit@gitlab.eu>
2017-03-22 12:39:40 +0200

    added always cascade with exceptions

--- lib/i18n/workflow.rb
+++ lib/i18n/workflow.rb
@@ -1,5 +1,6 @@
 require "i18n/workflow/version"
 require "i18n/workflow/always_cascade"
+require "i18n/workflow/always_cascade_with_exceptions"
 require "i18n/workflow/exception_handler"
 require "i18n/workflow/explicit_scope_key"
 require "i18n/workflow/translation_helper"
---
+++ lib/i18n/workflow/always_cascade_with_exceptions.rb
@@ -0,0 +1,8 @@
+# Module to make cascading in translations the default option
+module I18n::Workflow::AlwaysCascadeWithExceptions
+  def lookup(locale, key, scope = [], options = {})
+    # :unauthorized is cancancan scope, cascade caused tricky issues
+    options = options.merge(cascade: true) unless [:unauthorized].include?(scope)
+    super(locale, key, scope, options)
+  end
+end
