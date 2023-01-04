8088c794b1b0cda636d4c56eb0ba0506e1c53fd6
Priit Tark <priit@domify.io>
2023-01-04 15:06:11 +0200

    Allow cascade without scopes enabled

--- lib/i18n/workflow/always_cascade_with_exceptions.rb
+++ lib/i18n/workflow/always_cascade_with_exceptions.rb
@@ -1,8 +1,9 @@
 # Module to make cascading in translations the default option
 module I18n::Workflow::AlwaysCascadeWithExceptions
   def lookup(locale, key, scope = [], options = {})
+    
     # :unauthorized is cancancan scope, cascade caused tricky issues
-    options = options.merge(cascade: true) unless [:unauthorized].include?(scope.first)
+    options = options.merge(cascade: true) unless [:unauthorized].include?(scope&.first)
 
     super(locale, key, scope, options)
   end
